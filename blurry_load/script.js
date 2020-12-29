68905d575a9b7c3513a413d584548cd62746605b
Denis Frolov <d.fr8938@gmail.com>
2020-12-29 13:24:28 +0300

    add javascript code

---
+++ blurry_load/script.js
@@ -0,0 +1,24 @@
+let bg = document.querySelector('.bg');
+let loadText = document.querySelector('.loading_text');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+
+    if(load > 99) {
+        clearInterval(int);
+    }
+
+    loadText.innerHTML = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+    console.log(load);
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+let scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}  
